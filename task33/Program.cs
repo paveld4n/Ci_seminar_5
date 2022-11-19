0e4d4d9cc2f9c56d89cf724ed9516804ab5a5598
Pavel <rut100500@yandex.ru>
2022-11-19 22:44:55 +0300

    задать массив и найти в нем заданное число. С bool

---
+++ task33/Program.cs
@@ -0,0 +1,46 @@
+﻿// задать массив и найти в нем заданное число
+
+Console.WriteLine("ВВведите искомое число: ");
+bool isParsedS = int.TryParse(Console.ReadLine(), out int s);
+Console.WriteLine("ВВведите размерность массива: ");
+bool isParsedN = int.TryParse(Console.ReadLine(), out int n);
+if(!isParsedS || !isParsedN)
+{
+    Console.WriteLine("Не правильно введены данные!");
+    return;
+}
+
+int[] numbers = new int[n];
+FillArray (numbers);
+PrintArray (numbers);
+Console.WriteLine();
+
+Console.WriteLine($"искомое число = {FindNum(numbers, s)}");
+
+bool FindNum (int[] array, int num)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] == num)
+        return true;
+    }
+    return false;
+}
+
+void FillArray(int[] array)
+{
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(-10, 11);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($" {array[i]}");
+    }
+}
+
