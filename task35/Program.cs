ac44550c2c203ab953189ee91e5d965b9c801b6a
Pavel <rut100500@yandex.ru>
2022-11-19 22:47:40 +0300

    Найти количество элементов в отрезке от 10 до 99

---
+++ task35/Program.cs
@@ -0,0 +1,41 @@
+﻿//  Задать одномерный массив из 123 чисел. Найти количество элементов значения которых лежат в отрезке от 10 до 99
+
+
+
+int[] numbers = new int[123];
+FillArray (numbers);
+PrintArray (numbers);
+Console.WriteLine();
+int count = FindNum(numbers);
+
+Console.WriteLine($"количество элементов = {count}");
+
+int FindNum (int[] array)
+{
+    int res = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] >= 10 & array[i] <= 99)
+        {
+            res++;
+        }
+    }
+    return res;
+}
+
+void FillArray(int[] array)
+{
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(-1000, 1000);
+    }
+}
+void PrintArray(int[] array)
+{
+    Console.WriteLine();
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
